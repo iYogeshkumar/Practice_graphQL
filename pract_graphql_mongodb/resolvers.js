fa2ef43bbdba50042b43ec1f6328976055682ccf
yogesh Kumar <iamyogeshkumar21@gmail.com>
2023-03-06 22:11:46 +0530

    Update resolvers.js

--- pract_graphql_mongodb/resolvers.js
+++ pract_graphql_mongodb/resolvers.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import {users,quotes} from './db.js';
 import {randomBytes} from 'crypto'
 const User = mongoose.model("User")
-import bcrypt from 'bcrypt.js';
+import bcrypt from 'bcryptjs';
 //resolver
 const resolvers ={
     Query:{
@@ -26,12 +26,18 @@ const resolvers ={
     },
 
     Mutation:{
-        signupUser:(_,{userNew})=>{
+        signupUser:async(_,{userNew})=>{
              //if user is already exist
-          const user=User.findOne({email:userNew.email})          
+          const user= await User.findOne({email:userNew.email})          
           if(user){
             throw new Error("user already exist with that email")
           }
+          const hashedPassword= await bcrypt.hash(userNew.password,12)
+          const newUser= new User({
+            ...userNew,
+            password:hashedPassword
+          })
+          return await newUser.save()
         }
     }
 }
